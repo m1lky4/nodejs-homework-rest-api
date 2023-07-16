c4a9bfdb5091259ad0c5ad9f51fa47b64aee2446
m1lky4 <temanafanin1337@gmail.com>
2023-07-16 12:48:04 +0200

    fixed contactsPath after review

--- models/contacts.js
+++ models/contacts.js
@@ -2,7 +2,8 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const contactsPath = path.join(__dirname, 'contacts.json');
+const contactsPath = path.join(__dirname, 'models', 'contacts.json');
+
 
 const listContacts = async () => {
   try {
